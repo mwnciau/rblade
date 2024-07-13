535a21052b3286ee432cef03a9abd18ec6d59274
Simon J <simon@crml.uk>
2024-07-13 13:52:33 +0100

    Process arguments in tokenizes statements

--- lib/compiler/concerns/tokenizes_statements.rb
+++ lib/compiler/concerns/tokenizes_statements.rb
@@ -1,10 +1,12 @@
+require "ripper"
+
 class TokenizesStatements
   def self.tokenize!(tokens)
     tokens.map! do |token|
       next(token) if token.type != :unprocessed
 
       segments = token.value.split /\B(@@?)(\w+(?:::\w+)?)(?:[ \t]*(\(.*?\)))?/m
-      segments_orig = segments.map(&:clone)
+
       i = 0
       while i < segments.count do
         segment = segments[i]
@@ -14,17 +16,7 @@ class TokenizesStatements
 
           i += 1
         elsif segment == "@"
-          statementData = {statement: segments[i+1]}
-          segments.delete_at i + 1
-
-          if i + 1 < segments.count && segments[i + 1][0] == "("
-            arguments = segments[i + 1]
-
-            statementData[:arguments] = arguments
-            segments.delete_at i + 1
-          end
-
-          segments[i] = Token.new(type: :statement, value: statementData)
+          self.tokenizeStatement! segments, i
 
           i += 1
         elsif segments[i] != nil && segments[i] != ""
@@ -35,10 +27,107 @@ class TokenizesStatements
           segments.delete_at i
         end
       end
-      #dd segments_orig, segments
+
       segments
     end.flatten!
   end
 
+  def self.tokenizeStatement!(segments, i)
+    statementData = {statement: segments[i+1]}
+    segments.delete_at i + 1
+
+    if i + 1 < segments.count && segments[i + 1][0] == "("
+      arguments = self.tokenizeArguments! segments, i + 1
+
+      if !arguments.nil?
+        statementData[:arguments] = arguments
+      end
+    end
+
+    segments[i] = Token.new(type: :statement, value: statementData)
+  end
+  private_class_method :tokenizeStatement!
+
+  def self.tokenizeArguments!(segments, segmentIndex)
+    success = self.expandSegmentToEndParenthesis! segments, segmentIndex
+
+    # If no matching parentheses were found, so we combine the argument string with the next segment
+    if !success
+      if !segments[segmentIndex + 1].nil?
+        segments[segmentIndex] <<= segments[segmentIndex + 1]
+        segments.delete_at segmentIndex + 1
+      end
+
+      return nil
+    end
+
+    arguments = self.extractArguments segments[segmentIndex]
+    segments.delete_at segmentIndex
+
+    arguments
+  end
+  private_class_method :tokenizeArguments!
+
+  def self.expandSegmentToEndParenthesis! segments, segmentIndex
+    parenthesesDifference = 0
+    tokens = nil
+
+    loop do
+      tokens = Ripper.lex(segments[segmentIndex]).map {|token| token[1]}
+      parenthesesDifference = tokens.count(:on_lparen) - tokens.count(:on_rparen)
+
+      break if parenthesesDifference.zero? || segments[segmentIndex + 1].nil?
+
+      index = segments[segmentIndex + 1].each_char.find_index { |c| c == ")" && (parenthesesDifference = parenthesesDifference-1).zero? }
+
+      if index.nil?
+        segments[segmentIndex] << segments[segmentIndex + 1]
+        segments.delete_at segmentIndex + 1
+      else
+        segments[segmentIndex] << segments[segmentIndex + 1].slice!(0..index)
+      end
+
+      break if segments[segmentIndex + 1].nil?
+    end
+
+    parenthesesDifference.zero?
+  end
+  private_class_method :expandSegmentToEndParenthesis!
+
+  def self.extractArguments(segment)
+    # Add a comma to the end to delimit the end of the last argument
+    segment = segment[1..-2] + ','
+    segmentLines = segment.lines
+
+    tokens = Ripper.lex segment
+    arguments = []
+
+    currentLine = 1
+    currentIndex = 0
+    tokens.each do |token|
+      if token[1] == :on_comma
+        argument = ""
+
+        # Concatenate all lines up to this token's line, including the tail end of the current line
+        if token[0][0] != currentLine
+          for i in currentLine...token[0][0]
+            argument << (segmentLines[i - 1].slice(currentIndex..-1) || "")
+            currentIndex = 0
+          end
+          currentLine = token[0][0]
+        end
+        argument <<= segmentLines[currentLine - 1].slice(currentIndex...token[0][1])
+
+        arguments.push argument.strip
+
+        currentIndex = token[0][1] + 1
+      end
+    end
+
+    return nil if arguments.count == 1 && arguments.first == ""
+
+    arguments
+  end
+  private_class_method :extractArguments
 
 end
--- test/unit/compiler/concerns/tokenize_statements_test.rb
+++ test/unit/compiler/concerns/tokenize_statements_test.rb
@@ -12,18 +12,45 @@ class EchoTest < TestCase
     end
   end
 
-  def test_echo_variable
+  def test_tokenize
     assert_tokenizes_to "@foo", [{:statement => "foo"}]
     assert_tokenizes_to "@foo::bar", [{:statement => "foo::bar"}]
     assert_tokenizes_to "@foo @bar", [{:statement => "foo"}, " ", {:statement => "bar"}]
-    assert_tokenizes_to "@@foo", ["@@foo"]
-    assert_tokenizes_to "@foo()", [{:statement => "foo", :arguments => "()"}]
-    assert_tokenizes_to "@foo(1, 2, 3)", [{:statement => "foo", :arguments => "(1, 2, 3)"}]
-    assert_tokenizes_to "@foo   (1, 2, 3)", [{:statement => "foo", :arguments => "(1, 2, 3)"}]
+    assert_tokenizes_to "@foo()", [{:statement => "foo"}]
+    assert_tokenizes_to "@foo(1, 2, 3)", [{:statement => "foo", :arguments => ["1", "2", "3"]}]
+    assert_tokenizes_to "@foo   (1, 2, 3)", [{:statement => "foo", :arguments => ["1", "2", "3"]}]
     assert_tokenizes_to "@foo   (1,
     2,
-    3)", [{:statement => "foo", :arguments => "(1,
-    2,
-    3)"}]
+    3)", [{:statement => "foo", :arguments => ["1", "2", "3"]}]
+  end
+
+  def test_skip_statement
+    assert_tokenizes_to "@@foo", ["@@foo"]
+    assert_tokenizes_to "@@foo::bar", ["@@foo::bar"]
+    assert_tokenizes_to "@@foo(1, 2, 3)", ["@@foo", "(1, 2, 3)"]
+  end
+
+  def test_bracket_matching
+    assert_tokenizes_to "@foo()", [{:statement => "foo"}]
+    assert_tokenizes_to "@foo(')', 2)", [{:statement => "foo", :arguments => ["')'", "2"]}]
+    assert_tokenizes_to "@foo('(', 2)", [{:statement => "foo", :arguments => ["'('", "2"]}]
+    assert_tokenizes_to "@foo(%q[)], 2)", [{:statement => "foo", :arguments => ["%q[)]", "2"]}]
+
+    assert_tokenizes_to "@foo((1), ((2)), (3))", [{:statement => "foo", :arguments => ["(1)", "((2))", "(3)"]}]
+    assert_tokenizes_to "@foo((1 + 2), 3)", [{:statement => "foo", :arguments => ["(1 + 2)", "3"]}]
+    assert_tokenizes_to "@foo(#{'('*100}#{')'*100})", [{:statement => "foo", :arguments => ["#{'('*100}#{')'*100}"]}]
+
+    assert_tokenizes_to "@foo(1,
+    2)", [{:statement => "foo", :arguments => ["1", "2"]}]
+    assert_tokenizes_to "@foo(1
+, 2)", [{:statement => "foo", :arguments => ["1", "2"]}]
+    assert_tokenizes_to "@foo(1,
+    (2 + (3)),
+    4) @bar()", [{:statement => "foo", :arguments => ["1", "(2 + (3))", "4"]}, " ", {:statement => "bar"}]
+
+    assert_tokenizes_to "(@foo())", ["(", {:statement => "foo"}, ")"]
+    assert_tokenizes_to "@foo)", [{:statement => "foo"}, ")"]
+    assert_tokenizes_to "@foo(", [{:statement => "foo"}, "("]
+    assert_tokenizes_to "@foo(()(()", [{:statement => "foo"}, "(()(()"]
   end
 end
