317c6c00990a52a0a9326a996393731f84f04504
Simon J <simon@crml.uk>
2024-07-13 14:44:20 +0100

    Use pp for dd

--- lib/compiler/blade_compiler.rb
+++ lib/compiler/blade_compiler.rb
@@ -2,21 +2,13 @@ require_relative "concerns/compiles_comments"
 require_relative "concerns/compiles_echos"
 require_relative "concerns/tokenizes_statements"
 require "htmlentities"
+require "pp"
 
 Token = Struct.new(:type, :value)
 
 def dd *args
-  print "\n\nDump and die output:\n"
-  args.each do |arg|
-    if arg.is_a? Array
-      print "(Array[#{arg.count}]) [\n"
-      arg.each { |item| print "  (#{item.class}) #{item.inspect}\n" }
-      print "]\n"
-    else
-      print "(#{arg.class}) #{arg.inspect}\n"
-    end
-  end
-  print "\n"
+  print "\n\ndd output:\n"
+  pp(*args)
   exit 0
 end
 
