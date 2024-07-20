35dcbfc9ae21810c8262f39711c8fe2410a9409b
Simon Jarrett <simon.jarrett@nesta.org.uk>
2024-07-20 16:17:08 +0100

    Rename components test

--- test/unit/compiler/concerns/compiles_components_test.rb
+++ test/unit/compiler/concerns/compiles_components_test.rb
@@ -2,7 +2,7 @@ require_relative "../../../test_case"
 require_relative "../../../../lib/compiler/blade_compiler"
 
 class CompilesComponentsTest < TestCase
-  def test_end
+  def test_components
     assert_compiles_to "<x-button>hello</x-button>", nil, '<button class="button">hello</button>'
     assert_compiles_to "<x-button/>", nil, '<button class="button"></button>'
     assert_compiles_to "<x-link href=#>visit</x-link>", nil, '<a href="#">visit</a>'
