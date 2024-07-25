28e8e93f7f05941b905b72b4588037a044a95bb5
Simon Jarrett <2857218+mwnciau@users.noreply.github.com>
2024-07-25 19:25:40 +0100

    Fix @each with key value pairs

--- lib/rblade/compiler/statements/compiles_loops.rb
+++ lib/rblade/compiler/statements/compiles_loops.rb
@@ -22,26 +22,28 @@ module RBlade
       end
 
       def compileEach args
-        if args.nil? || args&.count > 2
+        if args.nil? || args.count > 2
           raise StandardError.new "Each statement: wrong number of arguments (given #{args&.count || 0}, expecting 1)"
         end
         # Allow variables to be a key, value pair
         args = args.join ","
 
-        variables, collection = args[0].split(" in ")
+        variables, collection = args.split(" in ")
 
         "#{collection}.each do |#{variables}|;"
       end
 
       def compileEachElse args
-        if args&.count != 1
-          raise StandardError.new "Each else statement: wrong number of arguments (given #{args&.count || 0}, expecting 1)"
+        if args.nil? || args.count > 2
+          raise StandardError.new "Each statement: wrong number of arguments (given #{args&.count || 0}, expecting 1)"
         end
+        # Allow variables to be a key, value pair
+        args = args.join ","
         @loop_else_counter += 1
 
-        variables, collection = args[0].split(" in ")
+        variables, collection = args.split(" in ")
 
-        "_looped_#{@loop_else_counter}=true;#{collection}.each do |#{variables}|;_looped_#{@loop_else_counter}=false;"
+        "_looped_#{@loop_else_counter}=false;#{collection}.each do |#{variables}|;_looped_#{@loop_else_counter}=true;"
       end
 
       def compileFor args
@@ -58,13 +60,13 @@ module RBlade
         end
         @loop_else_counter += 1
 
-        "_looped_#{@loop_else_counter}=true;for #{args[0]};_looped_#{@loop_else_counter}=false;"
+        "_looped_#{@loop_else_counter}=false;for #{args[0]};_looped_#{@loop_else_counter}=true;"
       end
 
       def compileEmpty
         @loop_else_counter -= 1
 
-        "end;if _looped_#{@loop_else_counter + 1};"
+        "end;if !_looped_#{@loop_else_counter + 1};"
       end
 
       def compileNext args
--- test/unit/compiler/concerns/statements/compiles_loops_test.rb
+++ test/unit/compiler/concerns/statements/compiles_loops_test.rb
@@ -47,17 +47,17 @@ class CompilesLoopsTest < TestCase
 
   def test_for_else
     assert_compiles_to "@forelse(i in []) {{ i }} @empty 0 @endforelse",
-      "_looped_1=true;for i in [];_looped_1=false;_out<<h(i);end;if _looped_1;_out<<'0';end;",
+      "_looped_1=false;for i in [];_looped_1=true;_out<<h(i);end;if !_looped_1;_out<<'0';end;",
       "0"
 
     assert_compiles_to "@forelse(i in 1..1) {{ i }} @empty 0 @endforelse",
-      "_looped_1=true;for i in 1..1;_looped_1=false;_out<<h(i);end;if _looped_1;_out<<'0';end;",
+      "_looped_1=false;for i in 1..1;_looped_1=true;_out<<h(i);end;if !_looped_1;_out<<'0';end;",
       "1"
   end
 
   def test_nested_for_else
     assert_compiles_to "@forelse(i in 1..1) @forelse(i in []) {{ i }} @empty 8 @endforelse @empty 9 @endforelse",
-      "_looped_1=true;for i in 1..1;_looped_1=false;_looped_2=true;for i in [];_looped_2=false;_out<<h(i);end;if _looped_2;_out<<'8';end;end;if _looped_1;_out<<'9';end;",
+      "_looped_1=false;for i in 1..1;_looped_1=true;_looped_2=false;for i in [];_looped_2=true;_out<<h(i);end;if !_looped_2;_out<<'8';end;end;if !_looped_1;_out<<'9';end;",
       "8"
   end
 
@@ -69,7 +69,7 @@ class CompilesLoopsTest < TestCase
 
   def test_eachelse
     assert_compiles_to "@eachelse(a in []) {{ a }} @empty 0 @endeach",
-      "_looped_1=true;[].each do |a|;_looped_1=false;_out<<h(a);end;if _looped_1;_out<<'0';end;",
+      "_looped_1=false;[].each do |a|;_looped_1=true;_out<<h(a);end;if !_looped_1;_out<<'0';end;",
       "0"
   end
 end
